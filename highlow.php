79c5f569287d3e90360ec3276c736b6677716c6b
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-04 10:59:50 -0600

    Forgot to commit after every step, but every step is present still.

--- highlow.php
+++ highlow.php
@@ -1,2 +1,25 @@
 <?php
+$random = mt_rand (1, 100);
+$input = 1;
+$guesses = 0;
+while ($input != $random && $input != "exit" && $input != 0) {
+	fwrite(STDOUT, "Take a Guess: ");
+	$input = trim(fgets(STDIN));
+	if ($input == "exit" || $input == 0) {
+		echo "You're a quitter\n";
+	} else if ($input < $random) {
+		fwrite(STDOUT, "HIGHER\n");
+		$guesses++;
+	} else if ($input > $random) {
+		fwrite(STDOUT, "LOWER\n");
+		$guesses++;
+	} 
+}
+
+if ($input == $random) {
+	echo "GOOD GUESS!!!!!\n";
+	echo "Number of Guesses: {$guesses}\n";
+}
+
+
 ?>
