6a024be6ef2161e222f8db9122e6e8d22c207f55
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-04 14:48:47 -0600

    cleaned up the code b/c of demo

--- highlow.php
+++ highlow.php
@@ -1,5 +1,9 @@
 <?php
 
+$min = 1;
+$max = 100;
+
+
 if ($argc == 3) {
 	if (is_numeric($argv[1]) && is_numeric($argv[2])) {
 		if($argv[1] > $argv[2]) {
@@ -13,38 +17,32 @@ if ($argc == 3) {
 		die("Please enter a valid argument\n");
 	}
 	
-} else {
-	$min = 1;
-	$max = 100;
 }
 
 $random = mt_rand ($min, $max);
-$input = 0.5;
 $guesses = 0;
 
 echo "I'm thinking of a number between $min and $max. What is it?\n";
-while ($input != $random && $input != "exit") {
+do {
 
 	fwrite(STDOUT, "Take a Guess: ");
 	$input = trim(fgets(STDIN));
+	$guesses++;
 
 	if ($input == "exit") {
-		$guesses++;
 		echo "You're a quitter!\n";
 		echo "Number of guesses it took you to give up: {$guesses}\n";
-	} else if ($input == 0) {
+	} else if (!is_numeric($input)) {
 		echo "That is not a valid guess\n";
 	} else if ($input < $random) {
 		echo "HIGHER\n";
 	} else if ($input > $random) {
 		echo "LOWER\n";
 	} else if ($input == $random) {
-		$guesses++;
 		echo "GOOD GUESS!!!!!\n";
 		echo "Number of Guesses: {$guesses}\n";
 	}
-	$guesses++;
-}
+} while ($input != $random && $input != "exit");
 
 
 ?>
